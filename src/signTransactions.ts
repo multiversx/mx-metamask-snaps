95be482c1a188022ef854835c160f07c5e51c47c
Andrei <giura.andrei70@gmail.com>
2024-05-28 22:59:24 +0300

    F-2024-2830 | Floating Point Precision and Rounding Errors

--- src/signTransactions.ts
+++ src/signTransactions.ts
@@ -1,12 +1,11 @@
-import { SignTransactionsParams } from './types/snapParam';
-import { IPlainTransactionObject, Transaction } from '@multiversx/sdk-core/out';
-import { getWalletKeys } from './private-key';
-import { denominate } from './denominate';
-import { DECIMALS, DIGITS, EGLD_LOGO } from './constants';
-import { getNetworkConfig, getNetworkType } from './network';
-import { NetworkType } from './types/networkType';
-import { NetworkConfig } from '@multiversx/sdk-network-providers/out';
-import { UserSecretKey } from '@multiversx/sdk-wallet/out';
+import { SignTransactionsParams } from "./types/snapParam";
+import { IPlainTransactionObject, Transaction } from "@multiversx/sdk-core/out";
+import { getWalletKeys } from "./private-key";
+import { DECIMALS, DIGITS, EGLD_LOGO } from "./constants";
+import { getNetworkConfig, getNetworkType } from "./network";
+import { NetworkType } from "./types/networkType";
+import { NetworkConfig } from "@multiversx/sdk-network-providers/out";
+import { UserSecretKey } from "@multiversx/sdk-wallet/out";
 import {
   divider,
   image,
@@ -15,24 +14,25 @@ import {
   text,
   copyable,
   RowVariant,
-} from '@metamask/snaps-sdk';
+} from "@metamask/snaps-sdk";
+import { formatAmount } from "./operations";
 
 /**
  * @param params - The transaction(s) to sign.
  */
 export const signTransactions = async (
-  transactionsParam: SignTransactionsParams,
+  transactionsParam: SignTransactionsParams
 ): Promise<string[]> => {
   const { userSecret } = await getWalletKeys();
 
   if (transactionsParam.transactions.length == 0) {
-    throw new Error('There must be atleast one transaction');
+    throw new Error("There must be atleast one transaction");
   }
 
   const chainId = transactionsParam.transactions[0].chainID;
 
   if (!validateSameChainId(transactionsParam.transactions, chainId)) {
-    throw new Error('All transactions must have the same chainId.');
+    throw new Error("All transactions must have the same chainId.");
   }
 
   const networkType = getNetworkType(chainId);
@@ -43,7 +43,7 @@ export const signTransactions = async (
 
   const networkConfig = await getNetworkConfig(networkType.apiAddress);
   if (!networkConfig) {
-    throw new Error('Cannot retrieve the network configuration from the api.');
+    throw new Error("Cannot retrieve the network configuration from the api.");
   }
 
   const transactionsSigned: string[] = [];
@@ -55,7 +55,7 @@ export const signTransactions = async (
       networkType,
       networkConfig,
       userSecret,
-      transactionsSigned,
+      transactionsSigned
     );
   }
 
@@ -66,11 +66,11 @@ export const signTransactions = async (
     networkType: NetworkType,
     networkConfig: NetworkConfig,
     userSecret: UserSecretKey,
-    transactionsSigned: string[],
+    transactionsSigned: string[]
   ) {
     const txValue = formatEGLD(
       transaction.getValue().toString(),
-      networkType.egldLabel,
+      networkType.egldLabel
     );
     const fees = transaction.computeFee(networkConfig).toString();
     const txFees = formatEGLD(fees, networkType.egldLabel);
@@ -78,11 +78,11 @@ export const signTransactions = async (
     const confirmationResponse = await showConfirmationDialog(
       transaction,
       txValue,
-      txFees,
+      txFees
     );
 
     if (confirmationResponse !== true) {
-      throw new Error('All transactions must be approved by the user');
+      throw new Error("All transactions must be approved by the user");
     }
 
     const serializedTransaction = transaction.serializeForSigning();
@@ -95,23 +95,23 @@ export const signTransactions = async (
   async function showConfirmationDialog(
     transaction: Transaction,
     txValue: string,
-    txFees: string,
+    txFees: string
   ): Promise<string | boolean | null> {
     return snap.request({
-      method: 'snap_dialog',
+      method: "snap_dialog",
       params: {
-        type: 'confirmation',
+        type: "confirmation",
         content: panel([
-          text('Send to'),
+          text("Send to"),
           text(transaction.getReceiver().bech32()),
           divider(),
-          text('Amount'),
+          text("Amount"),
           row(txValue, image(EGLD_LOGO), RowVariant.Default),
           divider(),
-          text('Fee'),
+          text("Fee"),
           row(txFees, image(EGLD_LOGO), RowVariant.Default),
           divider(),
-          text('Data'),
+          text("Data"),
           copyable(transaction.getData().toString()),
         ]),
       },
@@ -120,19 +120,19 @@ export const signTransactions = async (
 
   function validateSameChainId(
     transactions: IPlainTransactionObject[],
-    targetChainId: string,
+    targetChainId: string
   ): boolean {
     return transactions.every(
-      (transaction) => transaction.chainID === targetChainId,
+      (transaction) => transaction.chainID === targetChainId
     );
   }
 
   function formatEGLD(input: string, ticker: string) {
-    return denominate({
+    return formatAmount({
       input: input,
-      denomination: DECIMALS,
-      decimals: DIGITS,
       ticker: ticker,
+      decimals: DECIMALS,
+      digits: DIGITS,
     });
   }
 };
